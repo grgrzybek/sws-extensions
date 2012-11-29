416b7208f1bc67cc38126e7d60332d77193d3cb1
Grzegorz Grzybek <gr.grzybek@gmail.com>
2012-11-29 13:33:01 +0100

    Sample RPC/Encoded interface with soap messages

---
+++ src/test/java/org/springframework/ws/jaxws/client/core/sei/RpcEncodedBare01.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2005-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.ws.jaxws.client.core.sei;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
+
+/**
+ * <p></p>
+ *
+ * @author Grzegorz Grzybek
+ */
+@WebService
+@SOAPBinding(style = Style.RPC, use = Use.ENCODED, parameterStyle = ParameterStyle.BARE)
+public interface RpcEncodedBare01 {
+
+	/**
+	 * @param hello
+	 * @return
+	 */
+	@WebMethod
+	public String hello(@WebParam String arg0);
+
+}
